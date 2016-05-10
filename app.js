d38e3e46d4a2f1df4958f358d5224e322accec2f
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-05-10 14:26:32 +0700

    Add coffees

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require('express')
 var app     = express()
 app.get("/", home)
+app.get('/list', list)
 app.listen(2000)
 
 function home(req, res) {
   res.send("<i>Welcome to iOffer</i>")
 }
+
+function list(req, res) {
+  res.send(coffees)
+}
+
+var coffees = [
+  {name:'Latte',      price:80},
+  {name:'Americano',  price:70},
+  {name:'Cappuccino', price:90},
+  {name:'Espresso',   price:60}
+]
