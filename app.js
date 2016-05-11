33dc652d1637f2fefc67c308223be5fa89134795
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-05-11 15:24:29 +0700

    Support post new user

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express')
 var app     = express()
+var parser  = require('body-parser')
 
 app.use(express.static('public'))
+app.use(parser.urlencoded())
+
 app.set('view engine', 'ejs')
 app.get("/", home)
-app.get("/register", register)
+app.get ("/register", register)
+app.post("/register", registerMember)
 app.get('/list', list)
 app.listen(2000)
 
@@ -14,6 +18,11 @@ function home(req, res) {
 
 function register(req, res) {
 	res.render("register")
+}
+
+function registerMember(req, res) {
+	console.log(req.body)
+	res.render("index")
 }
 
 function list(req, res) {
