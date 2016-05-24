95954ab43e0b312cbca9679e1ec75e2500a714fd
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-05-24 15:55:47 +0700

    Update mobile app

--- mobile.js
+++ mobile.js
@@ -3,13 +3,26 @@ import React from 'react-native'
 class App extends React.Component {
   constructor() {
     super()
+    this.data = [ ]
+    fetch('http://ioffer.space:2000/search-result/หนังสือ')
+    .then( r => r.json() )
+    .then( d => {
+        this.data = d
+        this.setState({})
+      }
+    )
   }
 
   render() {
+    let items = [ ]
+    for (let r of this.data) {
+      items.push(<React.Text>{r.name}</React.Text>)
+    }
     return (
       <React.View>
         <React.Text>
         Welcome to React Native, the easiest way to write application for iOS and Android.
+        {items}
         </React.Text>
       </React.View>
     )
