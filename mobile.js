939471a67626db5622b1277cd54053a875aca49f
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-05-25 13:34:56 +0700

    Add search button to mobile app

--- mobile.js
+++ mobile.js
@@ -14,12 +14,35 @@ class App extends React.Component {
   }
 
   render() {
+    let appStyle = {
+      paddingTop: 30
+    }
+    let inputStyle = {
+      height: 30,
+      borderWidth: 1,
+      borderColor: 'lightgray'
+    }
+    let buttonStyle = {
+      borderWidth: 1,
+      borderColor: 'darkgray',
+      borderRadius: 4,
+      margin: 10,
+      padding: 8,
+      backgroundColor: 'lightblue'
+    }
+    let buttonTextStyle = {
+      textAlign: 'center' 
+    }
     let items = [ ]
     for (let r of this.data) {
       items.push(<Text>{r.name}</Text>)
     }
     return (
-      <View>
+      <View style={appStyle}>
+        <TextInput style={inputStyle}></TextInput>
+        <TouchableOpacity style={buttonStyle}>
+          <Text style={buttonTextStyle}>Search</Text>         
+        </TouchableOpacity>
         <Text>
         Welcome to React Native, the easiest way to write application for iOS and Android.
         {items}
