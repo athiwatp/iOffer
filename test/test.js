e94179a965ef60541c15ab84c387313fc354f24e
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-05-27 15:02:24 +0700

    Add the main test file

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var exec = require('child_process').exec;
+
+var scripts = [
+	"test001.js",
+	"test002.js"
+];
+
+for (var i = 0; i < scripts.length; i++) {
+	exec("phantomjs " + scripts[i], (e1, result, e2) => {
+		console.log(result);
+	});
+}
